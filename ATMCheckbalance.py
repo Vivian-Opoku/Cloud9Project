1d04a2653af46db51ec2a631d4f654afb3b5ed82
Vivian-Opoku <91670950+Vivian-Opoku@users.noreply.github.com>
2021-10-10 19:52:17 +0000

    Create ATMCheckbalance.py

---
+++ ATMCheckbalance.py
@@ -0,0 +1,4 @@
+# This code allows the user or the account holder to check his/her balance
+def check_balance(self):
+        print("\033[34mAvailable balance: \033[32mGHC", self.balance)
+        print()
